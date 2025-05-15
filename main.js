45846105305d0033d69eba14c9fe38d9f7b32907
marirods <mariane.rodrigues933@gmail.com>
2025-05-15 09:27:26 -0300

    CRUD revisado e finalizado

--- main.js
+++ main.js
@@ -348,10 +348,9 @@ async function postClassificacao(classificacoes){
 
 
 const novaClassificacao = {
-    "nome": "Nexus Dreams",
-    "fabricante": "DreamCircuit",
-    "dispositivo": "PC, SmartTV"
-  }
+    "descricao": "speed-drive",
+     "classificacao": "adulto"
+}
 
 // postJogo(novoJogo)
 
@@ -387,11 +386,9 @@ async function putClassificacao(id, classificacoes){
 }
 
 const updateClassificacao = {
-    "nome": "Nexus Dreams",
-    "fabricante": "DreamCircuit",
-    "dispositivo": "Tablet, SmartTV"
-  }
-
+    "descricao": "RPG",
+     "classificacao": "Adulto"
+}
 
 async function deleteClassificacao(id){
     // console.log(contato)
@@ -404,5 +401,85 @@ async function deleteClassificacao(id){
         //ok é verdadeiro se deu certo e falso deu errado
         return response.ok
 }
+
+/********************************************************************************************************************************************************************************* */
+
+//categoria
+
+async function postCategoria(categorias){
+    // console.log(contato)
+      const url = 'http://localhost:3030/v1/controle-categorias/categorias'
+      const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(categorias)
+      }
+      const response = await fetch(url, options)
+
+      //ok é verdadeiro se deu certo e falso deu errado
+      return response.ok
+}
+
+
+
+const novaCategoria = {
+    "nome": "Daniela",
+     "genero": "Masculino"
+}
+
+// postJogo(novoJogo)
+
+ async function getCategoria(){
+    const url = 'http://localhost:3030/v1/controle-categorias/categorias'
+    const response = await fetch(url)
+    const data = await response.json()
+    return data
+}
+
+async function getCategoriaById(id){
+    const url = `http://localhost:3030/v1/controle-categorias/categorias/${id}`
+        const response = await fetch(url)
+        const data = await response.json()
+        console.log(data)
+        return data
+    }
+    
+async function putCategoria(id, categoria){
+    // console.log(contato)
+        const url = `http://localhost:3030/v1/controle-categorias/categorias/${id}`
+        const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(categoria)
+        }
+        const response = await fetch(url, options)
+
+        //ok é verdadeiro se deu certo e falso deu errado
+        return response.ok
+}
+
+const updateCategoria = {
+    "nome": "Daniela",
+     "genero": "Feminino"
+}
+
+
+async function deleteCategoria(id){
+    // console.log(contato)
+        const url = `http://localhost:3030/v1/controle-categorias/categorias/delete/${id}`
+        const options = {
+        method: 'DELETE',
+        }
+        const response = await fetch(url, options)
+
+        //ok é verdadeiro se deu certo e falso deu errado
+        return response.ok
+}
+
+
 
 
